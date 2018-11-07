8634759688f4163d419085086721ec047940a215
Mahadi Hassan <36642308+itsmahadi007@users.noreply.github.com>
2018-11-07 17:58:38 +0600

    Update The Artful Expedient.cpp

--- Codeforce/The Artful Expedient.cpp
+++ Codeforce/The Artful Expedient.cpp
@@ -1,1 +1,45 @@
 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<int,bool>arr;
+
+    int n;
+    cin>>n;
+    int a1[n],a2[n];
+
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a1[i]);
+        arr[a1[i]]=1;
+    }
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a2[i]);
+        arr[a2[i]]=1;
+    }
+    int temp=0,coun=0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            temp=a1[i]^a2[j];
+            if(arr.find(temp)!=arr.end())
+            {
+                coun++;
+            }
+        }
+    }
+    if(coun%2== 0)
+    {
+        cout<<"Karen"<<endl;
+    }
+    else
+        cout<<"Koyomi"<<endl;
+
+    return 0;
+}
